fb7f0f08df5ff71c83a151e67365e3777b8ca39e
chang2694 <115095264+chang2694@users.noreply.github.com>
2023-06-11 22:43:12 +0700

    add execute folder

--- DiditalSignature/signature.c
+++ DiditalSignature/signature.c
@@ -8,121 +8,130 @@
 
 int main(void)
 {
-#pragma region CreateKey
-	//crypto_sign_keypair(pk, sk);
-
-	//// Public key
-	//FILE* file;
-	//errno_t err = fopen_s(&file, "publickey.key", "wb"); // Mở tệp để ghi dữ liệu nhị phân (binary) với fopen_s
-
-	//if (err != 0) {
-	//	printf("Không thể mở tệp.\n");
-	//	return 1;
-	//}
-
-	//size_t keySize = sizeof(pk); // Kích thước của key trong byte
-
-	//size_t elementsWritten = fwrite(pk, sizeof(uint8_t), keySize, file); // Ghi key vào tệp
-
-	//if (elementsWritten != keySize) {
-	//	printf("Lỗi khi ghi dữ liệu.\n");
-	//	fclose(file);
-	//	return 1;
-	//}
-
-	//fclose(file);
-	//printf("Đã ghi key vào tệp publickey.key.\n");
-
-	//// Secret key
-	//err = fopen_s(&file, "secretkey.key", "wb"); // Mở tệp để ghi dữ liệu nhị phân (binary) với fopen_s
-
-	//if (err != 0) {
-	//	printf("Không thể mở tệp.\n");
-	//	return 1;
-	//}
-
-	//keySize = sizeof(sk); // Kích thước của key trong byte
-
-	//elementsWritten = fwrite(sk, sizeof(uint8_t), keySize, file); // Ghi key vào tệp
-
-	//if (elementsWritten != keySize) {
-	//	printf("Lỗi khi ghi dữ liệu.\n");
-	//	fclose(file);
-	//	return 1;
-	//}
-
-	//fclose(file);
-	//printf("Đã ghi key vào tệp secretkey.key.\n");
-#pragma endregion
-
-	char fileName[5];
-	fgets(fileName, 5, stdin);
-
-	char filePath[100];
-	strcpy_s(filePath, sizeof(filePath), "D:\\DigitalSignature\\Dilithium\\DiditalSignature\\FilePDF\\");
-	strcat_s(filePath, sizeof(filePath), fileName);
-
-	int option = 2;
+	int option = 1;
 	if (option == 1) {
-		size_t mlen, smlen;
-		uint8_t* m;
-
+#pragma region CreateKey
+		uint8_t pk[CRYPTO_PUBLICKEYBYTES];
 		uint8_t sk[CRYPTO_SECRETKEYBYTES];
-		readKeyFromFile("secretkey.key", sk, sizeof(sk));
+		crypto_sign_keypair(pk, sk);
 
+		// Public key
+		FILE* file;
+		errno_t err = fopen_s(&file, "publickey.key", "wb"); // Mở tệp để ghi dữ liệu nhị phân (binary) với fopen_s
 
-		strcat_s(filePath, sizeof(filePath), ".pdf");
-
-		size_t dataSize;
-		m = readPDF(filePath, &dataSize);
-		if (!m) {
-			printf("Failed to read file: %s\n", filePath);
-			return 0;
+		if (err != 0) {
+			printf("Không thể mở tệp.\n");
+			return 1;
 		}
 
-		uint8_t* sm = calloc(dataSize + CRYPTO_BYTES, sizeof(uint8_t));
+		size_t keySize = sizeof(pk); // Kích thước của key trong byte
 
-		crypto_sign(sm, &smlen, m, dataSize, sk);
+		size_t elementsWritten = fwrite(pk, sizeof(uint8_t), keySize, file); // Ghi key vào tệp
 
-		char signedFilePath[100];
-		strcpy_s(signedFilePath, sizeof(signedFilePath), "D:\\DigitalSignature\\Dilithium\\DiditalSignature\\FilePDF\\");
-		strcat_s(signedFilePath, sizeof(filePath), fileName);
-		strcat_s(signedFilePath, sizeof(signedFilePath), "_signed.pdf");
-		writeFile(signedFilePath, sm, smlen);
-		free(m);
-	 }
+		if (elementsWritten != keySize) {
+			printf("Lỗi khi ghi dữ liệu.\n");
+			fclose(file);
+			return 1;
+		}
+
+		fclose(file);
+
+		// Secret key
+		err = fopen_s(&file, "secretkey.key", "wb"); // Mở tệp để ghi dữ liệu nhị phân (binary) với fopen_s
+
+		if (err != 0) {
+			printf("Không thể mở tệp.\n");
+			return 1;
+		}
+
+		keySize = sizeof(sk); // Kích thước của key trong byte
+
+		elementsWritten = fwrite(sk, sizeof(uint8_t), keySize, file); // Ghi key vào tệp
+
+		if (elementsWritten != keySize) {
+			printf("Lỗi khi ghi dữ liệu.\n");
+			fclose(file);
+			return 1;
+		}
+
+		fclose(file);
+#pragma endregion
+	}
 	else {
-	int ret;
-	size_t mlen, smlen;
-	uint8_t* sm;
+		char fileName[50];
+		fgets(fileName, 50, stdin);
 
-	uint8_t pk[CRYPTO_PUBLICKEYBYTES];
-	readKeyFromFile("publickey.key", pk, sizeof(pk));
+		size_t newlinePos = strcspn(fileName, "\n");
 
-	strcat_s(filePath, sizeof(filePath), "_signed.pdf");
-	size_t dataSize;
-	sm = readPDF(filePath, &dataSize);
-	if (!sm) {
-		printf("Failed to read file: %s\n", filePath);
+		if (newlinePos < strlen(fileName)) {
+			fileName[newlinePos] = '\0';
+		}
+
+		if (option == 2) {
+			size_t mlen, smlen;
+			uint8_t* m;
+
+			uint8_t sk[CRYPTO_SECRETKEYBYTES];
+			readKeyFromFile("secretkey.key", sk, sizeof(sk));
+
+			char filePath[50];
+			strcpy_s(filePath, sizeof(filePath), fileName);
+			strcat_s(filePath, sizeof(filePath), ".pdf");
+
+			size_t dataSize;
+
+			m = readPDF(filePath, &dataSize);
+			if (!m) {
+				printf("Failed to read file: %s\n", filePath);
+				return 0;
+			}
+
+			uint8_t* sm = calloc(dataSize + CRYPTO_BYTES, sizeof(uint8_t));
+
+			crypto_sign(sm, &smlen, m, dataSize, sk);
+
+			char signedFilePath[50];
+			strcpy_s(signedFilePath, sizeof(signedFilePath), fileName);
+			strcat_s(signedFilePath, sizeof(signedFilePath), "_signed.pdf");
+			writeFile(signedFilePath, sm, smlen);
+			free(m);
+		}
+		else {
+			int ret;
+			size_t mlen, smlen;
+			uint8_t* sm;
+
+			uint8_t pk[CRYPTO_PUBLICKEYBYTES];
+			readKeyFromFile("publickey.key", pk, sizeof(pk));
+
+			char signedFilePath[50];
+			strcpy_s(signedFilePath, sizeof(signedFilePath), fileName);
+			strcat_s(signedFilePath, sizeof(signedFilePath), "_signed.pdf");
+			size_t dataSize;
+			sm = readPDF(signedFilePath, &dataSize);
+			if (!sm) {
+				printf("Failed to read file: %s\n", signedFilePath);
+				return 0;
+			}
+			mlen = dataSize - CRYPTO_BYTES;
+			smlen = dataSize;
+
+			uint8_t* m = calloc(mlen, sizeof(uint8_t));
+			for (int i = 0; i < mlen; ++i)
+				m[mlen - 1 - i] = sm[CRYPTO_BYTES + mlen - 1 - i];
+
+			ret = crypto_sign_open(&mlen, sm, smlen, pk);
+			if (ret)
+			{
+				fprintf(stderr, "Verification failed\n");
+				return -1;
+			}
+
+			free(sm);
+		}
+	}
+		system("pause");
 		return 0;
-	}
-	mlen = dataSize - CRYPTO_BYTES;
-	smlen = dataSize;
 
-	uint8_t* m = calloc(mlen, sizeof(uint8_t));
-	for (int i = 0; i < mlen; ++i)
-		m[mlen - 1 - i] = sm[CRYPTO_BYTES + mlen - 1 - i];
-
-	ret = crypto_sign_open(&mlen, sm, smlen, pk);
-	if (ret)
-	{
-		fprintf(stderr, "Verification failed\n");
-		return -1;
-	}
-
-	free(sm);
-	}
-	system("pause");
-	return 0;
-
+	
 }
