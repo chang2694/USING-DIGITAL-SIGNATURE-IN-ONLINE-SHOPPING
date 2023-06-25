1de835cd6c4e54ecf0bc04d13d63a4584b2fb08e
baoipc <baofam1906@gmail.com>
2023-06-25 23:49:07 +0700

    Bổ sung phần code dựng database

---
+++ TestDatabase/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace TestDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.readBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.UserNameTB = new System.Windows.Forms.TextBox();
+            this.PasswordTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.BackColor = System.Drawing.Color.RosyBrown;
+            this.connectBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectBtn.Location = new System.Drawing.Point(38, 38);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(145, 85);
+            this.connectBtn.TabIndex = 0;
+            this.connectBtn.Text = "Kết nối";
+            this.connectBtn.UseVisualStyleBackColor = false;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // readBtn
+            // 
+            this.readBtn.BackColor = System.Drawing.Color.RosyBrown;
+            this.readBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.readBtn.Location = new System.Drawing.Point(297, 38);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(145, 85);
+            this.readBtn.TabIndex = 1;
+            this.readBtn.Text = "Xem data";
+            this.readBtn.UseVisualStyleBackColor = false;
+            this.readBtn.Click += new System.EventHandler(this.readBtn_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.BackColor = System.Drawing.Color.RosyBrown;
+            this.updateBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateBtn.Location = new System.Drawing.Point(553, 38);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(145, 85);
+            this.updateBtn.TabIndex = 2;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = false;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // UserNameTB
+            // 
+            this.UserNameTB.Location = new System.Drawing.Point(179, 273);
+            this.UserNameTB.Name = "UserNameTB";
+            this.UserNameTB.Size = new System.Drawing.Size(262, 22);
+            this.UserNameTB.TabIndex = 3;
+            // 
+            // PasswordTB
+            // 
+            this.PasswordTB.Location = new System.Drawing.Point(179, 341);
+            this.PasswordTB.Name = "PasswordTB";
+            this.PasswordTB.Size = new System.Drawing.Size(262, 22);
+            this.PasswordTB.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tài khoản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordTB);
+            this.Controls.Add(this.UserNameTB);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.readBtn);
+            this.Controls.Add(this.connectBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Button readBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.TextBox UserNameTB;
+        private System.Windows.Forms.TextBox PasswordTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestDatabase/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using MySqlConnector;
+using Azure.Identity;
+using System.Security.Cryptography;
+
+namespace TestDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public static string ComputeSHA256(string s)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Cần chuyển đổi string sang dạng byte khi Hash
+                byte[] hashValue = sha256.ComputeHash(Encoding.UTF8.GetBytes(s));
+                // Chuyển đổi chuỗi vừa hash sang dạng string để dễ sử dụng
+                return BitConverter.ToString(hashValue).Replace("-", "").ToLower();
+            }
+        }
+
+        private async void connectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var builder = new MySqlConnectionStringBuilder
+                {
+                    Server = "databaseforusing.mysql.database.azure.com",
+                    Database = "testsql",
+                    UserID = "user1admin",
+                    Password = "m4tkh4u!123",
+                    //SslMode = MySqlSslMode.Required,
+                };
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    MessageBox.Show("Đã connect", "Thông báo");
+                    await connection.OpenAsync();
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "DROP TABLE IF EXISTS UserAdmin;";
+                        await command.ExecuteNonQueryAsync();
+                        //Console.WriteLine("Finished dropping table (if existed)");
+
+                        command.CommandText = "CREATE TABLE UserAdmin (id serial PRIMARY KEY, user VARCHAR(50), password VARCHAR(50));";
+                        await command.ExecuteNonQueryAsync();
+                        //Console.WriteLine("Finished creating table");
+
+                        command.CommandText = @"INSERT INTO UserAdmin (user, password) VALUES (@user, @password),
+                        (@name2, @quantity2), (@name3, @quantity3);";
+                        command.Parameters.AddWithValue("@user1", UserNameTB.Text);
+                        command.Parameters.AddWithValue("@password1", ComputeSHA256(PasswordTB.Text));
+                        
+                        MessageBox.Show("Đã add data", "Thông báo");
+                        int rowCount = await command.ExecuteNonQueryAsync();
+                        MessageBox.Show(String.Format("Number of rows inserted={0}", rowCount));
+                    }
+                }
+            }
+            catch (SqlException xe)
+            {
+                MessageBox.Show(xe.ToString());
+            }
+            //
+            //Console.ReadLine();
+        }
+
+        private async void readBtn_Click(object sender, EventArgs e)
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "databaseforusing.mysql.database.azure.com",
+                Database = "testsql",
+                UserID = "user1admin",
+                Password = "m4tkh4u!123",
+                //SslMode = MySqlSslMode.Required,
+            };
+
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                MessageBox.Show("Opening connection");
+                await conn.OpenAsync();
+
+                using (var command = conn.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM UserAdmin;";
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            MessageBox.Show(string.Format(
+                                "Reading from table=({0}, {1}, {2})",
+                                reader.GetInt32(0),
+                                reader.GetString(1),
+                                reader.GetInt32(2)));
+                        }
+                    }
+                    
+                }
+
+                MessageBox.Show("Closing connection");
+            }
+        }
+
+        private async void updateBtn_Click(object sender, EventArgs e)
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "databaseforusing.mysql.database.azure.com",
+                Database = "testsql",
+                UserID = "user1admin",
+                Password = "m4tkh4u!123",
+                //SslMode = MySqlSslMode.Required,
+            };
+
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                MessageBox.Show("Opening connection");
+                await conn.OpenAsync();
+
+                using (var command = conn.CreateCommand())
+                {
+                    command.CommandText = "UPDATE inventory SET quantity = @quantity WHERE name = @name;";
+                    command.Parameters.AddWithValue("@quantity", 200);
+                    command.Parameters.AddWithValue("@name", "banana");
+
+                    int rowCount = await command.ExecuteNonQueryAsync();
+                    MessageBox.Show(String.Format("Number of rows updated={0}", rowCount));
+                }
+
+                MessageBox.Show("Closing connection");
+            }
+        }
+    }
+}
